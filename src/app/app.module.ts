e359f79e68b520fb8a557bf224a1c35f99c2e289
Shubham Vohra <shubham.vohra@geminisolutions.com>
2022-09-06 18:37:38 +0530

    header and left menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Header/header.component';
+import { LeftmenuComponent } from './LeftMenu/leftmenu.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LeftmenuComponent
   ],
   imports: [
     BrowserModule,
