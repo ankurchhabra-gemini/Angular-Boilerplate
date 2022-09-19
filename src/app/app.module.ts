1d7ff4297ededb03046f9aa3c31c9da999f01905
Balbir Kaur <ba.kaur@gemsol.in>
2022-09-19 16:29:08 +0530

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Header/header.component';
 import { LeftmenuComponent } from './LeftMenu/leftmenu.component';
+import { MatToolbarModule } from  '@angular/material/toolbar';
+import { MatSidenavModule } from  '@angular/material/sidenav';
+import { MatButtonModule } from  '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
 
 
 @NgModule({
@@ -15,7 +20,13 @@ import { LeftmenuComponent } from './LeftMenu/leftmenu.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
